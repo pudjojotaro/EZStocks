dec8962d49c9de85526e0c580085ffbc43d8148a
pudjo_jotaro <123965179+pudjojotaro@users.noreply.github.com>
2024-06-06 13:05:50 -0400

    updated creds access to be private
    now in a separate file

--- core/message_puller.py
+++ core/message_puller.py
@@ -1,19 +1,55 @@
+import asyncio
 from pyrogram import Client # type: ignore
+from pymongo import MongoClient # type: ignore
+from pymongo.mongo_client import MongoClient # type: ignore
+from pymongo.server_api import ServerApi # type: ignore
+from urllib.parse import quote_plus
+import json
 
-api_id = '22125082'  # Replace with your actual api_id
-api_hash = '46f9d3e4015771def24972cb08d86ce6'  # Replace with your actual api_hash
+# Load credentials and configuration from JSON file
+with open("credentials.json", "r") as file:
+    creds = json.load(file)
 
-app = Client("my_session", api_id=api_id, api_hash=api_hash)
+# MongoDB Connection Setup
+username = creds["username"]
+password = creds["password"]
+cluster_url = creds["cluster_url"]
 
-async def get_chat_IDS():
-    async with app:
-        async for dialog in app.get_dialogs():
-            # Printing both the title (or first name, if it's a user chat) and the chat ID
-            print(f"Name: {dialog.chat.title or dialog.chat.first_name}, ID: {dialog.chat.id}")
+client = MongoClient(
+    f"mongodb+srv://{username}:{password}@{cluster_url}/?retryWrites=true&w=majority&appName=Cluster0",
+    server_api=ServerApi('1')
+)
 
+db = client['telegram']
+collection = db['messages']
 
+# Pyrogram Setup
+api_id = creds["api_id"]
+api_hash = creds["api_hash"]
+app = Client("testsession", api_id=creds["api_id"], api_hash=creds["api_hash"])
 
 async def main():
-    await get_chat_IDS()
+    channel_id = -1001893826477
+    async with app:
+        async for message in app.get_chat_history(channel_id):
+            # Extract message details
+            message_data = {
+                "channel_id": channel_id,
+                "date": message.date,
+                "message_id": message.id,
+                "reply_to_message_id": message.reply_to_message_id,
+                "text": message.text
+            }
+            # Insert message into MongoDB
+            try:
+                collection.insert_one(message_data)
+                print(f"Inserted message {message.id} into MongoDB.")
+            except Exception as e:
+                print(f"Failed to insert message {message.id}: {e}")
+            
+            # Sleep to control the rate of message fetching
+            await asyncio.sleep(2)
 
 app.run(main())
+
+
